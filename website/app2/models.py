bd929545a2fa0239ea16f9827e70fc8d940eae34
nilesh05apr <nk8588338@gmail.com>
2021-06-20 10:46:37 +0530

    database and some basic functionality

--- website/app2/models.py
+++ website/app2/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField(blank = False, null = False, upload_to = "media/")
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.pk)
--- website/app2/views.py
+++ website/app2/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+from .models import Post
 # Create your views here.
 
 def display(request):
     return render(request,'app2/index.html')
+
+class Home(TemplateView):
+    template_name = 'index.html'
+
+def file_upload(request):
+    if request.method == 'POST':
+        my_file = request.FILES.get('file')
+        Post.objects.create(image=my_file)
+        return HttpResponse('')
+    return JsonResponse({'post':'false'})

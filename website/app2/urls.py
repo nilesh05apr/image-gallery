79f0e181525b4dedac531f4fc70ccaa80948ef5c
nilesh05apr <nk8588338@gmail.com>
2022-08-07 21:38:40 +0530

    changed css

--- website/app2/urls.py
+++ website/app2/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('',views.display_post,name='display_post'),
     path('add',views.manage_post, name='manage_post'),
-    path('<int:p_id>',views.delete_post,name='delete'),
+    path('delete/<int:p_id>',views.delete_post,name='delete'),
     path('like/<int:p_id>',views.increment_likes,name='like')
 ]
